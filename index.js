52f22760072f1e49562c03a2d3df7611649a92bd
sara altaee <sara.korea96@gmail.com>
2020-06-07 17:15:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+function saturdayFun(activity="roller-skate"){
+ return console.log(`This Saturday, I want to !${activity}`);
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+function mondayWork(target="go to the office"){
+  console.log(`This Monday, I will${target}` );
+}
+mondayWork();
+mondayWork("I willwork from home");
+
+function wrapAdjective(star="*"){
+  return (m="special")=>{
+    return console.log(`You are ${star} ${m} ${star}`);
+  }
+}
+
+let calculator{
+   add: ()=>1+3;
+   subtract: ()=> 1-3;
+    multiply:()=> 1*3;
+    divide: ()=> 10/5;
+}
